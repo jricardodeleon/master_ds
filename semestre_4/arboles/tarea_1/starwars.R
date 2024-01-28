9320cc7fef5df7fbdfb95a91fcafbc650739e7f5
De Leon Flores, Ricardo Javier <josericardo.deleon@astrazeneca.com>
2024-01-28 15:30:31 -0600

    adding semestre_4 folder

---
+++ semestre_4/arboles/tarea_1/starwars.R
@@ -0,0 +1,22 @@
+# This is an Analysis about the dataset starwars
+# Ricardo De León
+# Tarea 1
+# Materia Árboles
+
+# load required librearies
+library(tidyverse)
+library(dplyr)
+
+# load dataset
+starwars_df <- as.data.frame(dplyr::starwars) 
+
+# 
+
+
+
+
+# install.packages("tidyverse")
+# install.packages("gridExtra")
+# install.packages("grid")
+# install.packages("DataExplorer")
+#install.packages("nycflights13")
